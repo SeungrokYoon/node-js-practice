23257ae2c2dbeb9241d7cf4da7defd727eb02fdf
SeungrokYoon <sonicrok93@gmail.com>
2024-02-11 23:41:48 +0900

    feat: 136.http 모듈 response객체와 fs모듈 활용

--- http.js
+++ http.js
@@ -1,18 +1,19 @@
 const http = require("http");
+const fs = require("fs");
 const server = http
   .createServer((req, res) => {
-    res.writeHead(200, { "Content-Type": "text/html" });
-    res.end("Hello world");
+    /**
+     * 서버에서는 이렇게 파일을 읽어서 Res 객체에 담아 응답해주면,
+     * 브라우저는 이것을 파싱해서 화면에 띄워 주는구나.
+     */
+    fs.readFile("./html/index.html", (err, data) => {
+      res.writeHead(200, { "Content-Type": "text/html" });
+      res.end(data);
+    });
   })
   .listen(50000, () => {
     console.log("서버가 동작 중입니다");
   });
-
-const testClose = function () {
-  server.close(() => {
-    console.log("test 서버가 종료되었습니다");
-  });
-};
 
 server.on("request", () => {
   console.log("request");
@@ -25,5 +26,3 @@ server.on("connection", () => {
 server.on("close", () => {
   console.log("서버가 종료되었습니다.");
 });
-
-setTimeout(testClose, 10000);
