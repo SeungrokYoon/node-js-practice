d5a65579431a45a61c110849dc1916339a072d45
SeungrokYoon <sonicrok93@gmail.com>
2024-02-29 14:10:33 +0900

    feat: cookie 추출

--- get/request.get.js
+++ get/request.get.js
@@ -3,12 +3,13 @@ const url = require("url");
 
 http
   .createServer((req, res) => {
-    console.log(req.protocol);
     const url = new URL(req.url, "https://127.0.0.1:50000");
-    console.log(url);
     if (req.method === "GET") {
-      res.writeHead(200, { "Content-Type": "text/html" });
-      res.end(`<h1>${JSON.stringify(url.searchParams.toString())}</h1>`);
+      res.writeHead(200, {
+        "Content-Type": "text/html",
+        "Set-Cookie": ["drink=coffee", "desert=cake"],
+      });
+      res.end(`<h1>${req.headers.cookie}</h1>`);
     } else if (req.method === "POST") {
       console.log(`${req.method}방식의 요청입니다.`);
     }
