8e8cd894f31a752c18b8d9c3432d096496541921
SeungrokYoon <sonicrok93@gmail.com>
2024-03-03 23:50:29 +0900

    feat: express에 static html파일 연결하여 제공

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
+const path = require("path");
 const express = require("express");
 const app = express();
 const port = 5500;
 
+app.use(express.static(path.join(__dirname, "./html")));
+
 app.get("/", (req, res) => {
-  res.send({ answer: "Hello world" });
+  res.send(path.join(__dirname, "./html/index.html"));
+});
+
+app.get("/error", (req, res) => {
+  res.status(404).send("<h1>404 ERROR</h1>");
 });
 
 app.listen(port, () => {
