f89f059bb801edc64699ccaf907d10c134f352da
SeungrokYoon <sonicrok93@gmail.com>
2024-03-25 11:30:37 +0900

    feat: express request모듈 사용, nodemon 추가

--- app.js
+++ app.js
@@ -3,7 +3,26 @@ const express = require("express");
 const app = express();
 const port = 5500;
 
-app.use(express.static(path.join(__dirname, "./html")));
+// app.use(express.static(path.join(__dirname, "./html")));
+app.use((request, response) => {
+  const agent = request.header("User-Agent");
+  const paramName = request.query.name;
+  const browserChrome = "Hello Chrome";
+  const browserOthers = "Hello Others";
+
+  console.log(request.headers);
+  console.log(request.baseUrl);
+  console.log(request.hostname);
+  console.log(request.protocol);
+
+  if (agent.toLowerCase().match(/chrome/)) {
+    response.write(`<div><p>${browserChrome}</p></div>`);
+  } else {
+    response.write(`<div><p>${browserOthers}</p></div>`);
+  }
+  response.write(`<div><p>${agent}</p></div>`);
+  response.end();
+});
 
 app.get("/", (req, res) => {
   res.send(path.join(__dirname, "./html/index.html"));
