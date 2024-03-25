be06eb875029afe0b866cd43fa0db1c8591f6b15
SeungrokYoon <sonicrok93@gmail.com>
2024-03-25 17:42:19 +0900

    feat: body-parser를 통한 form 데이터 받기

--- app.js
+++ app.js
@@ -1,11 +1,22 @@
 const path = require("path");
 const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
 const port = 5500;
 
 app.use(express.static(path.join(__dirname, "./html")));
 
+/**
+ * @description bodyParser.urlencoded extended false는
+ */
+// app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+/**
+ * 미들웨어를 여러개를 순차적으로 엮을 수 있다.
+ */
 app.use((request, response, next) => {
+  console.log(request.body);
   console.log("first middleware");
   request.user1 = "승록";
   next();
@@ -19,14 +30,30 @@ app.use((request, response, next) => {
 
 app.use((request, response, next) => {
   console.log("third middleware");
-  response.writeHead("200", { "Content-Type": "text/html;charset=utf8" });
-  response.write(`<div><p>${request.user1}</p></div>`);
-  response.write(`<div><p>${request.user2}</p></div>`);
-  response.end(`<h1>express서버에서 응답한 결과</h1>`);
+  next();
 });
 
 app.get("/", (req, res) => {
-  res.send(path.join(__dirname, "./html/index.html"));
+  res.sendFile(path.join(__dirname, "./html/index.html"));
+});
+
+app.get("/login", (req, res) => {
+  res.status(200).set("Content-Type", "text/html");
+  res.sendFile(path.join(__dirname, "./html/login.html"));
+});
+
+app.post("/login", (req, res) => {
+  const { userId, password } = req.body;
+  if (!userId || !password) {
+    return res.status(400).json({ error: "Missing id, pw" });
+  }
+  res.status(200).set("Content-Type", "application/json");
+  const jsonData = {
+    date: new Date(),
+    userId,
+    password,
+  };
+  res.json(jsonData);
 });
 
 app.get("/error", (req, res) => {
