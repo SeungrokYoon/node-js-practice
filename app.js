d980a076704978199d1bbf17eab7ebbcb2910b59
SeungrokYoon <sonicrok93@gmail.com>
2024-03-25 14:49:11 +0900

    feat: middleware test

--- app.js
+++ app.js
@@ -3,25 +3,26 @@ const express = require("express");
 const app = express();
 const port = 5500;
 
-// app.use(express.static(path.join(__dirname, "./html")));
-app.use((request, response) => {
-  const agent = request.header("User-Agent");
-  const paramName = request.query.name;
-  const browserChrome = "Hello Chrome";
-  const browserOthers = "Hello Others";
+app.use(express.static(path.join(__dirname, "./html")));
 
-  console.log(request.headers);
-  console.log(request.baseUrl);
-  console.log(request.hostname);
-  console.log(request.protocol);
+app.use((request, response, next) => {
+  console.log("first middleware");
+  request.user1 = "승록";
+  next();
+});
 
-  if (agent.toLowerCase().match(/chrome/)) {
-    response.write(`<div><p>${browserChrome}</p></div>`);
-  } else {
-    response.write(`<div><p>${browserOthers}</p></div>`);
-  }
-  response.write(`<div><p>${agent}</p></div>`);
-  response.end();
+app.use((request, response, next) => {
+  console.log("second middleware");
+  request.user2 = "승재";
+  next();
+});
+
+app.use((request, response, next) => {
+  console.log("third middleware");
+  response.writeHead("200", { "Content-Type": "text/html;charset=utf8" });
+  response.write(`<div><p>${request.user1}</p></div>`);
+  response.write(`<div><p>${request.user2}</p></div>`);
+  response.end(`<h1>express서버에서 응답한 결과</h1>`);
 });
 
 app.get("/", (req, res) => {
